5a76d889c19110a1fedd4849f7135feb96a73a78
Mayank Malhan <61201683+mayankmalhan123@users.noreply.github.com>
2022-02-20 13:41:10 +0400

    update day 7

---
+++ Day 7/Q1,2,3and4.js
@@ -0,0 +1,65 @@
+// Create a class Movie
+
+class Movie {
+    Constructor(title, studio, rating = 'PG') {
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+
+    getPG(arr) {
+        let result = [];
+        arr.filter((movie) => {
+            if(movie.rating === 'PG') {
+                result.push(movie)
+            }
+        })
+        return result;
+    }
+}
+
+let movie1 = new Movie('Casino Royale', 'Eon Productions', 'PG13');
+let movie2 = new Movie('Tenet', 'Warner Bros', 'PG');
+let movie3 = new Movie('Shawshank Redemption', 'Columbia Pictures', 'R');
+let movie4 = new Movie('Toy Story', 'Pixar Animation Studios', 'PG')
+let moviesArr = [movie1, movie2, movie3, movie4];
+console.log(movie1.getPG(moviesArr));
+
+//Write a “person” class to hold all the details.
+
+// class Person {
+//     Constructor(name, age, qualification, location) {
+//         this.name = name;
+//         this.age = age;
+//         this.qualification = qualification;
+//         this.location = location;
+//     }
+//     getDetails() {
+//         return `
+//         Name: ${this.name}
+//         Age: ${this.age}
+//         Qualification: ${this.qualification}
+//         Location: ${this.location}`
+//     }
+// }
+
+// let mayank = new Person('Mayank Malhan', 21, 'Bachelor of Technology Computer Science', 'Dubai')
+
+// console.log(mayank.getDetails());
+
+
+//write a class to calculate uber price.
+
+// class Uber {
+//     Constructor(distance, price) {
+//         this.distance = distance;
+//         this.price = price;
+//     }
+
+//     getPrice(){
+//         return this.distance*this.price;
+//     }
+// }
+
+// let taxi1 = new Uber(2, 100);
+// console.log(taxi1.getPrice());
