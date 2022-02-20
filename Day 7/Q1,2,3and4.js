fe8b7a187c109d77536d34cee840a222832f2b6f
Mayank Malhan <61201683+mayankmalhan123@users.noreply.github.com>
2022-02-20 13:47:50 +0400

    update

--- Day 7/Q1,2,3and4.js
+++ Day 7/Q1,2,3and4.js
@@ -1,7 +1,7 @@
 // Create a class Movie
 
 class Movie {
-    Constructor(title, studio, rating = 'PG') {
+    constructor(title, studio, rating = 'PG') {
         this.title = title;
         this.studio = studio;
         this.rating = rating;
@@ -25,41 +25,45 @@ let movie4 = new Movie('Toy Story', 'Pixar Animation Studios', 'PG')
 let moviesArr = [movie1, movie2, movie3, movie4];
 console.log(movie1.getPG(moviesArr));
 
+//Write a "circle" class 
+
+
+
 //Write a “person” class to hold all the details.
 
-// class Person {
-//     Constructor(name, age, qualification, location) {
-//         this.name = name;
-//         this.age = age;
-//         this.qualification = qualification;
-//         this.location = location;
-//     }
-//     getDetails() {
-//         return `
-//         Name: ${this.name}
-//         Age: ${this.age}
-//         Qualification: ${this.qualification}
-//         Location: ${this.location}`
-//     }
-// }
+class Person {
+    constructor(name, age, qualification, location) {
+        this.name = name;
+        this.age = age;
+        this.qualification = qualification;
+        this.location = location;
+    }
+    getDetails() {
+        return `
+        Name: ${this.name}
+        Age: ${this.age}
+        Qualification: ${this.qualification}
+        Location: ${this.location}`
+    }
+}
 
-// let mayank = new Person('Mayank Malhan', 21, 'Bachelor of Technology Computer Science', 'Dubai')
+let mayank = new Person('Mayank Malhan', 21, 'Bachelor of Technology Computer Science', 'Dubai')
 
-// console.log(mayank.getDetails());
+console.log(mayank.getDetails());
 
 
 //write a class to calculate uber price.
 
-// class Uber {
-//     Constructor(distance, price) {
-//         this.distance = distance;
-//         this.price = price;
-//     }
+class Uber {
+    constructor(distance, price) {
+        this.distance = distance;
+        this.price = price;
+    }
 
-//     getPrice(){
-//         return this.distance*this.price;
-//     }
-// }
+    getPrice(){
+        return this.distance*this.price;
+    }
+}
 
-// let taxi1 = new Uber(2, 100);
-// console.log(taxi1.getPrice());
+let taxi1 = new Uber(2, 100);
+console.log(taxi1.getPrice());
